77e35041b6effdb6d3087e210f67528bc2ce8ba3
Artem Borodai <artem.borodai@blinq.no>
2022-04-25 15:27:59 +0200

    test888

--- main.tf
+++ main.tf
@@ -115,42 +115,7 @@ resource "azuread_conditional_access_policy" "CP03" {
 
 }  
 
-resource "azuread_conditional_access_policy" "CP02" {
-  display_name = "CP02-Require-MFAAuthenticationforadmin-AllAps-Everywhere"
-  state        = "enabledForReportingButNotEnforced" 
 
-  conditions {
-    client_app_types    = ["all"]
-    sign_in_risk_levels = []
-    user_risk_levels    = []
-
-    applications {
-      included_applications = ["all"]
-      excluded_applications = []
-    }
-
-    locations {
-      included_locations = []
-      excluded_locations = []
-    }
-    platforms {
-      included_platforms = ["all"]
-      excluded_platforms = []
-    }
-    users {
-      included_users = []
-      included_roles = [var.global_admin_role]
-      excluded_users = []
-      excluded_groups = [var.emergency_admins]
-    }
-  }
-
-  grant_controls {
-    operator          = "OR"
-    built_in_controls = ["mfa"]
-  }
-
-}
 
 resource "azuread_conditional_access_policy" "CP04" {
   display_name = "CP03-Require-MFAAUthenticationforeveryone-AllAps-Everywherev333"
